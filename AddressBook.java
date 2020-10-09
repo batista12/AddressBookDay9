ca38cc3fedf95175d03b96d6bb847598a2e2c5b2
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-09 13:25:58 -0700

    [Manasi Singh] UC1 is created: UC1 branch is created

---
+++ AddressBook.java
@@ -0,0 +1,47 @@
+public class AddressBookMain {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String state;
+	private String zip;
+	private String phoneNo;
+	private String emailId;
+	/**
+	 * @param firstName
+	 * @param lastName
+	 * @param address
+	 * @param state
+	 * @param zip
+	 * @param phoneNo
+	 * @param emailId
+	 */
+	public AddressBookMain (String firstName, String lastName, String address, String state, String zip,
+			String phoneNo, String emailId)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNo = phoneNo;
+		this.emailId = emailId;
+	}
+	/**
+	 * 
+	 */
+	public void displayInfo(){
+		System.out.println("firstName: " + firstName);
+		System.out.println("lastName: " + lastName);
+		System.out.println("city: " + address);
+		System.out.println("state: " + state);
+		System.out.println("zip: " + zip);
+		System.out.println("phoneNumber: " + phoneNo);
+		System.out.println("email: " + emailId);
+	}
+	public static void main(String[] args) {
+		AddressBookMain contact = new AddressBookMain( "Manasi", "Singh", "svghdfgh",
+				"hjshghja","674384", "92324448391", "manasisingh2602@gmail.com");
+		contact.displayInfo();
+		System.out.println(contact);
+	}
+}
